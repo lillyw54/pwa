ceb5270b5389dad8c32e14e970c6d175f1e50be0
idilia wheat <lillyw54@gmail.com>
2024-01-15 15:24:08 -0600

    removed and added webpack.config.js

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,34 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+      }),
+      new WebpackPwaManifest({
+        name: 'Your Text Editor',
+        short_name: 'Text Editor',
+        description: 'A progressive web application for note-taking and code snippets.',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        start_url: '/',
+        icons: [
+          {
+            src: path.resolve('src/icons/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src/sw.js', // Path to your service worker file
+        swDest: 'service-worker.js',
+      }),
     ],
 
     module: {
       rules: [
-        
+        // Add your CSS loader configuration here if needed
+        // Add your Babel loader configuration here if needed
       ],
     },
   };
