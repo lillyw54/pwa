fda762e71d8fdef82636d1c1a4d3eb4d0a585713
idilia wheat <lillyw54@gmail.com>
2024-01-18 19:28:57 -0600

    changes

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -10,39 +10,60 @@ module.exports = () => {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+
     plugins: [
       new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
+        template: './index.html',
+        title: 'JATE'
       }),
-      new WebpackPwaManifest({
-        name: 'Your Text Editor',
-        short_name: 'Text Editor',
-        description: 'A progressive web application for note-taking and code snippets.',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        start_url: '/',
-        icons: [
-          {
-            src: path.resolve('src/icons/icon.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-          },
-        ],
-      }),
+
       new InjectManifest({
-        swSrc: './src/sw.js', // Path to your service worker file
-        swDest: 'service-worker.js',
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+         fingerprints: false,
+         inject: true,
+         name: 'Just Another Text Editor',
+         short_name: 'JATE',
+         description: 'a simple text editor that can be used online and offline',
+         background_color: '#225ca3',
+         theme_color: '#225ca3',
+         start_url: './',
+         publicPath: './',
+         icons: [
+      {
+        src: path.resolve('src/images/logo.png'),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join('assets', 'icons'),
+      },
+      ],
       }),
     ],
 
     module: {
       rules: [
-        // Add your CSS loader configuration here if needed
-        // Add your Babel loader configuration here if needed
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
